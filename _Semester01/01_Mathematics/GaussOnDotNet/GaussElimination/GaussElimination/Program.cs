ef27e14c7e001d6dd781b1d3d913c38a1891dacb
Abhijeet <abhijeet.sharma@wework.co.in>
2021-06-07 17:06:10 +0530

    Added code for Gauss elimination

---
+++ _Semester01/01_Mathematics/GaussOnDotNet/GaussElimination/GaussElimination/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GaussElimination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static float[] GaussElimination(float[] A, int d)
+        {
+            float[] x;
+
+            return x;
+        }
+    }
+}
